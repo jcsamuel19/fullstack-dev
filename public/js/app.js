1df259da5516da2764fd7854ba33e3d6b512c719
Jeremy Samuel <jeremysamuel@Jeremys-MacBook-Air.local>
2024-02-08 00:26:06 -0600

    Click button once and it updates color

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,52 @@
 class ParentComponent extends React.Component {
-    render(){
-        return (
-            <div className='ui unstackable items'>
-                 <ChildComponent/>
-            </div>
-        );
+    constructor(props) {
+      super(props);
+      this.state = {
+        items: ['Item 1', 'Item 2', 'Item 3']
+      };
     }
-}
-
-class ChildComponent extends React.Component {
+  
     render() {
-        return (
-            <div className='ui centered card'>
-                <div className='field'>
-                    <label>ChildComponent</label>
-                </div>
-                <button className='ui basic blue button'>
-                    Submit!
-                </button>
-            </div>
-        );
+      return (
+        <div className="ui unstackable items">
+          {this.state.items.map((item, index) => (
+            <ChildComponent key={index} item={item} changeColor={() => this.changeColor(index)} />
+          ))}
+        </div>
+      );
     }
-}
-
-ReactDOM.render(
-    <ParentComponent />,
-    document.getElementById("content")
-);
+  
+    changeColor(index) {
+        const items = [...this.state.items]; // Copy the items array
+        items[index] = <span style={{ color: this.getRandomColor() }}>{items[index]}</span>; // Change the color of the item at the specified index
+        this.setState({ items }); // Update the state with the new items array
+      }
+    
+      getRandomColor() {
+        // Generate a random color
+        return '#' + Math.floor(Math.random()*16777215).toString(16);
+      }
+    }
+  
+  class ChildComponent extends React.Component {
+    render() {
+      const { item } = this.props;
+      return (
+        <div className="ui centered card">
+          <div className="field">
+            <label>{item}</label>
+          </div>
+          <button className="change-color-btn" onClick={this.handleChangeColor}>
+            Click here to change color
+          </button>
+        </div>
+      );
+    }
+  
+    handleChangeColor = () => {
+      // Call the changeColor function passed from parent
+      this.props.changeColor();
+    }
+  }
+  
+  ReactDOM.render(<ParentComponent />, document.getElementById("content"));
